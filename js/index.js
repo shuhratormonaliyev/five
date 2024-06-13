acf620608b7201f8fffacba7f6c5f0d605d7ace8
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-06-14 02:02:38 +0500

    five

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// ======================== 1 - misol =====================
+
+
+// const words = ["1", "2", "3", "4", "5", "6", "7"];
+
+// const res = words.filter(word => word.length >= 5);
+
+// constresults = res.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+// console.log(result);
