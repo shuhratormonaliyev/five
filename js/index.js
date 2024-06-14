4009fbf383dbff6535d0d909896201a63ba11b8d
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-06-14 11:03:04 +0500

    five

--- js/index.js
+++ js/index.js
@@ -3,8 +3,62 @@
 
 // const words = ["1", "2", "3", "4", "5", "6", "7"];
 
-// const res = words.filter(word => word.length >= 5);
+// const res = words.filter(word => word.length => 5);
 
 // constresults = res.map(word => word.charAt(0).toUpperCase() + word.slice(1));
 
 // console.log(result);
+
+
+
+// ======================== 2 - misol =====================
+
+
+
+// const person = [
+//     { name: "ali", age: 18 },
+//     { name: "jamoliddin", age: 22 },
+//     { name: "shuhrat", age: 19 },
+//     { name: "alisher", age: 21 }
+// ];
+//   function name(stydent) {
+//     const stydent = stydent.find(stydent => stydent.ege > 20)
+//     if (stydent) {
+//         return stydent.name.toUpperCase();
+//     }
+//     return null;
+//   }
+//   const result = name(stydent);
+// console.log(result);
+
+
+// ======================== 3 - misol =====================
+
+// const person = [
+//         {nomi: "olma", soni: 10 },
+//         {nomi: "nok", soni: 80 },
+//         {nomi: "banan", soni: 113 },
+//         {nomi: "ananas", soni: 99 }
+//     ];
+
+//     function nomi(mevalar) {
+//         const mevalar = mevalar.filter(mevalar => mevalar.soni > 100 )
+//         if (mevalar) {
+//             return mevalar.nomi.reduce();
+//         }
+//         return null;
+//     }
+//     const result = nomi(mevalar);
+//     console.log(result);
+
+
+
+// ======================== 4 - misol =====================
+
+
+
+
+
+
+
+
